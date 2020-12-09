1a3092a5becdd290e76364b6b142464bfe1a503e
Lela Smith <smith.lelaa@gmail.com>
2020-12-08 17:08:24 -0800

    update deck of cards example

--- js/script.js
+++ js/script.js
@@ -1,55 +1,75 @@
-//Deck of Cards
+//Deck of Cards Example for Arrays and Looping lesson
 
+//With arrays and looping
 // $(document).ready(function() {
-  //   var cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
-  //   var suits = ["spades", "clubs", "hearts", "diamonds"];
-  //   cards.forEach(function(card) {
-    //     suits.forEach(function(suit) {
-      //       $(card + " of " + suit).text();
-      //       console.log(card + " of " + suit);
-      //     })
-      //   });
-      // });
 
+//   const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
+//   const suits = ["Spades", "Clubs", "Hearts", "Diamonds"];
 
+//   cards.forEach(function(card) {
+//     suits.forEach(function(suit) {
 
-      $(document).ready(function() {
-        $("form#deck ").submit(function(event) {
-          var text = $("input#userInput").val()
+//       $("#result").append("<li>" + card + " of " + suit + "</li >");
 
-          // console.log(text);
+//     });
+//   });
 
-          var wordArr = text.split(' ');
+// });
 
-          // console.log(wordArr);
+//Manually written deck of cards
+$(document).ready(function () {
 
-          // var words = $(wordArr.toString());
+  $("#result").append("<li> 2 of Spades  </li>");
+  $("#result").append("<li> 2 of Clubs </li>");
+  $("#result").append("<li> 2 of Hearts </li>");
+  $("#result").append("<li> 2 of Diamonds </li>");
+  $("#result").append("<li> 3 of Spades </li>");
+  $("#result").append("<li> 3 of Clubs </li>");
+  $("#result").append("<li> 3 of Hearts </li>");
+  $("#result").append("<li> 3 of Diamonds </li>");
+  $("#result").append("<li> 4 of Spades </li>");
+  $("#result").append("<li> 4 of Clubs </li>");
+  $("#result").append("<li> 4 of Hearts </li>");
+  $("#result").append("<li> 4 of Diamonds </li>");
+  $("#result").append("<li> 5 of Spades </li>");
+  $("#result").append("<li> 5 of Clubs </li>");
+  $("#result").append("<li> 5 of Hearts </li>");
+  $("#result").append("<li> 5 of Diamonds </li>");
+  $("#result").append("<li> 6 of Spades </li>");
+  $("#result").append("<li> 6 of Clubs </li>");
+  $("#result").append("<li> 6 of Hearts </li>");
+  $("#result").append("<li> 6 of Diamonds </li>");
+  $("#result").append("<li> 7 of Spades </li>");
+  $("#result").append("<li> 7 of Clubs </li>");
+  $("#result").append("<li> 7 of Hearts </li>");
+  $("#result").append("<li> 7 of Diamonds </li>");
+  $("#result").append("<li> 8 of Spades </li>");
+  $("#result").append("<li> 8 of Clubs </li>");
+  $("#result").append("<li> 8 of Hearts </li>");
+  $("#result").append("<li> 8 of Diamonds </li>");
+  $("#result").append("<li> 9 of Spades </li>");
+  $("#result").append("<li> 9 of Clubs </li>");
+  $("#result").append("<li> 9 of Hearts </li>");
+  $("#result").append("<li> 9 of Diamonds </li>");
+  $("#result").append("<li> 10 of Spades </li>");
+  $("#result").append("<li> 10 of Clubs </li>");
+  $("#result").append("<li> 10 of Hearts </li>");
+  $("#result").append("<li> 10 of Diamonds </li>");
+  $("#result").append("<li> Jack of Spades </li>");
+  $("#result").append("<li> Jack of Clubs </li>");
+  $("#result").append("<li> Jack of Hearts </li>");
+  $("#result").append("<li> Jack of Diamonds </li>");
+  $("#result").append("<li> Queen of Spades </li>");
+  $("#result").append("<li> Queen of Clubs </li>");
+  $("#result").append("<li> Queen of Hearts </li>");
+  $("#result").append("<li> Queen of Diamonds </li>");
+  $("#result").append("<li> King of Spades </li>");
+  $("#result").append("<li> King of Clubs </li>");
+  $("#result").append("<li> King of Hearts </li>");
+  $("#result").append("<li> King of Diamonds </li>");
+  $("#result").append("<li> Ace of Spades </li>");
+  $("#result").append("<li> Ace of Clubs </li>");
+  $("#result").append("<li> Ace of Hearts </li>");
+  $("#result").append("<li> Ace of Diamonds </li>");
 
-          // console.log(words)
-
-          var result = {};
-
-          wordArr.forEach(function(string) {
-          if (result[string]){
-          result[string] += 1
-          } else {
-            result[string] = 1
-
-          }
-           // result = (result + 1);
-           //
-           //  console.log(wordArr);
-           //
-           //  // console.log(string)
-           //
-           //  console.log(result)
-
-         }); console.log(result)
-          // text.forEach(function(word) {
-            //
-            //   $(".result").text(text);
-            // });
-            event.preventDefault();
-
-          });
-        });
+});
