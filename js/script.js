fdbc24655a4186ed699dbb7859a39ea1a3505688
Lela Smith <smith.lelaa@gmail.com>
2020-12-17 17:45:15 -0800

    clean up example

--- js/script.js
+++ js/script.js
@@ -1,75 +1,75 @@
 //Deck of Cards Example for Arrays and Looping lesson
 
 //With arrays and looping
-// $(document).ready(function() {
+$(document).ready(function() {
 
-//   const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
-//   const suits = ["Spades", "Clubs", "Hearts", "Diamonds"];
+  const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
+  const suits = ["Spades", "Clubs", "Hearts", "Diamonds"];
 
-//   cards.forEach(function(card) {
-//     suits.forEach(function(suit) {
+  cards.forEach(function(card) {
+    suits.forEach(function(suit) {
 
-//       $("#result").append("<li>" + card + " of " + suit + "</li >");
+      $("#result").append("<li>" + card + " of " + suit + "</li >");
 
-//     });
-//   });
-
-// });
-
-//Manually written deck of cards
-$(document).ready(function () {
-
-  $("#result").append("<li> 2 of Spades  </li>");
-  $("#result").append("<li> 2 of Clubs </li>");
-  $("#result").append("<li> 2 of Hearts </li>");
-  $("#result").append("<li> 2 of Diamonds </li>");
-  $("#result").append("<li> 3 of Spades </li>");
-  $("#result").append("<li> 3 of Clubs </li>");
-  $("#result").append("<li> 3 of Hearts </li>");
-  $("#result").append("<li> 3 of Diamonds </li>");
-  $("#result").append("<li> 4 of Spades </li>");
-  $("#result").append("<li> 4 of Clubs </li>");
-  $("#result").append("<li> 4 of Hearts </li>");
-  $("#result").append("<li> 4 of Diamonds </li>");
-  $("#result").append("<li> 5 of Spades </li>");
-  $("#result").append("<li> 5 of Clubs </li>");
-  $("#result").append("<li> 5 of Hearts </li>");
-  $("#result").append("<li> 5 of Diamonds </li>");
-  $("#result").append("<li> 6 of Spades </li>");
-  $("#result").append("<li> 6 of Clubs </li>");
-  $("#result").append("<li> 6 of Hearts </li>");
-  $("#result").append("<li> 6 of Diamonds </li>");
-  $("#result").append("<li> 7 of Spades </li>");
-  $("#result").append("<li> 7 of Clubs </li>");
-  $("#result").append("<li> 7 of Hearts </li>");
-  $("#result").append("<li> 7 of Diamonds </li>");
-  $("#result").append("<li> 8 of Spades </li>");
-  $("#result").append("<li> 8 of Clubs </li>");
-  $("#result").append("<li> 8 of Hearts </li>");
-  $("#result").append("<li> 8 of Diamonds </li>");
-  $("#result").append("<li> 9 of Spades </li>");
-  $("#result").append("<li> 9 of Clubs </li>");
-  $("#result").append("<li> 9 of Hearts </li>");
-  $("#result").append("<li> 9 of Diamonds </li>");
-  $("#result").append("<li> 10 of Spades </li>");
-  $("#result").append("<li> 10 of Clubs </li>");
-  $("#result").append("<li> 10 of Hearts </li>");
-  $("#result").append("<li> 10 of Diamonds </li>");
-  $("#result").append("<li> Jack of Spades </li>");
-  $("#result").append("<li> Jack of Clubs </li>");
-  $("#result").append("<li> Jack of Hearts </li>");
-  $("#result").append("<li> Jack of Diamonds </li>");
-  $("#result").append("<li> Queen of Spades </li>");
-  $("#result").append("<li> Queen of Clubs </li>");
-  $("#result").append("<li> Queen of Hearts </li>");
-  $("#result").append("<li> Queen of Diamonds </li>");
-  $("#result").append("<li> King of Spades </li>");
-  $("#result").append("<li> King of Clubs </li>");
-  $("#result").append("<li> King of Hearts </li>");
-  $("#result").append("<li> King of Diamonds </li>");
-  $("#result").append("<li> Ace of Spades </li>");
-  $("#result").append("<li> Ace of Clubs </li>");
-  $("#result").append("<li> Ace of Hearts </li>");
-  $("#result").append("<li> Ace of Diamonds </li>");
+    });
+  });
 
 });
+
+//Manually written deck of cards
+// $(document).ready(function () {
+
+//   $("#result").append("<li> 2 of Spades  </li>");
+//   $("#result").append("<li> 2 of Clubs </li>");
+//   $("#result").append("<li> 2 of Hearts </li>");
+//   $("#result").append("<li> 2 of Diamonds </li>");
+//   $("#result").append("<li> 3 of Spades </li>");
+//   $("#result").append("<li> 3 of Clubs </li>");
+//   $("#result").append("<li> 3 of Hearts </li>");
+//   $("#result").append("<li> 3 of Diamonds </li>");
+//   $("#result").append("<li> 4 of Spades </li>");
+//   $("#result").append("<li> 4 of Clubs </li>");
+//   $("#result").append("<li> 4 of Hearts </li>");
+//   $("#result").append("<li> 4 of Diamonds </li>");
+//   $("#result").append("<li> 5 of Spades </li>");
+//   $("#result").append("<li> 5 of Clubs </li>");
+//   $("#result").append("<li> 5 of Hearts </li>");
+//   $("#result").append("<li> 5 of Diamonds </li>");
+//   $("#result").append("<li> 6 of Spades </li>");
+//   $("#result").append("<li> 6 of Clubs </li>");
+//   $("#result").append("<li> 6 of Hearts </li>");
+//   $("#result").append("<li> 6 of Diaonds </li>");
+//   $("#result").append("<li> 7 of Spades </li>");
+//   $("#result").append("<li> 7 of Clubs </li>");
+//   $("#result").append("<li> 7 of Hearts </li>");
+//   $("#result").append("<li> 7 of Diamonds </li>");
+//   $("#result").append("<li> 8 of Spades </li>");
+//   $("#result").append("<li> 8 of Clubs </li>");
+//   $("#result").append("<li> 8 of Hearts </li>");
+//   $("#result").append("<li> 8 of Diamonds </li>");
+//   $("#result").append("<li> 9 of Spades </li>");
+//   $("#result").append("<li> 9 of Clubs </li>");
+//   $("#result").append("<li> 9 of Hearts </li>");
+//   $("#result").append("<li> 9 of Diamonds </li>");
+//   $("#result").append("<li> 10 of Spades </li>");
+//   $("#result").append("<li> 10 of Clubs </li>");
+//   $("#result").append("<li> 10 of Hearts </li>");
+//   $("#result").append("<li> 10 of Diamonds </li>");
+//   $("#result").append("<li> Jack of Spades </li>");
+//   $("#result").append("<li> Jack of Clubs </li>");
+//   $("#result").append("<li> Jack of Hearts </li>");
+//   $("#result").append("<li> Jack of Diamonds </li>");
+//   $("#result").append("<li> Queen of Spades </li>");
+//   $("#result").append("<li> Queen of Clubs </li>");
+//   $("#result").append("<li> Queen of Hearts </li>");
+//   $("#result").append("<li> Queen of Diamonds </li>");
+//   $("#result").append("<li> King of Spades </li>");
+//   $("#result").append("<li> King of Clubs </li>");
+//   $("#result").append("<li> King of Hearts </li>");
+//   $("#result").append("<li> King of Diamonds </li>");
+//   $("#result").append("<li> Ace of Spades </li>");
+//   $("#result").append("<li> Ace of Clubs </li>");
+//   $("#result").append("<li> Ace of Hearts </li>");
+//   $("#result").append("<li> Ace of Diamonds </li>");
+
+// });
